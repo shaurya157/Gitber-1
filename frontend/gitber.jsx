4fa3f9004bef7213cf57530e886fb5cfa64cd27f
Shaurya Sinha <shaurya157@gmail.com>
2017-07-02 18:29:08 +0800

    webpack, npm setup

---
+++ frontend/gitber.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store'
+import Root from './components/root'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const rootEl = document.getElementById('root');
+  let store;
+  if(window.currentUser){
+    store = configureStore({session: {currentUser: window.currentUser}})
+  } else {
+    store = configureStore();
+  }
+
+// debugging purposes
+  // window.store = store;
+  // window.success = (data) => console.log(data);
+  // window.error = data => console.log(data);
+  // window.ask = ask;
+  ReactDOM.render(<Root store={store} />, rootEl);
+});
